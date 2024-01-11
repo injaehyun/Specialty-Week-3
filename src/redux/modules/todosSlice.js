bae34b296398b9839409df242fd4e570034381ea
jaehyun <ijh9147@gmail.com>
2024-01-11 11:44:54 +0900

    주석 추가

--- src/redux/modules/todosSlice.js
+++ src/redux/modules/todosSlice.js
@@ -39,7 +39,8 @@ const todosSlice = createSlice({
   },
   extraReducers: (builder) => {
     //extraReducers 사용: reducers에서 바로구현되지 않는 기타 Reducer로직을 구현
-    // try catch로도 가능한가?
+    // extraReducers를 주석 처리하고 나면, 비동기 액션인 __addToDo 및 __deleteTodo의 성공 (fulfilled) 및 실패 (rejected) 케이스에 대한 상태 업데이트 로직이 
+    // 제거되므로 해당 비동기 액션의 성공 또는 실패 결과가 제대로 처리되지 않게됨
     builder
       .addCase(__addToDo.fulfilled, (state, action) => {
         state.list.push(action.payload);
